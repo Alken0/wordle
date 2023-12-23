160243e6776790231f4dce21b2e35a136d32029d
Jonas Bürgel <jonas.buergel@ise.fraunhofer.de>
2023-12-23 17:50:33 +0100

    generate correct files for wordle

--- data/main.py
+++ data/main.py
@@ -1,20 +1,47 @@
+import linecache
 import re
 
-words = []
 
-with open('sources/wordle-at.txt') as file:
-    data = file.read().replace('\n', '')
-    words += re.findall(r"'([A-Z]{5})'", data)
-    print(f"words after adding wordle-at: {len(words)}")
+def generate_words():
+    words = []
 
-with open('sources/wordle-de.txt') as file:
-    data = file.read().replace('\n', '')
-    words += re.findall(r'"([A-Z]{5})"', data)
-    print(f"words after adding wordle-de: {len(words)}")
+    with open('sources/wordle-at.txt') as file:
+        data = file.read().replace('\n', '')
+        words += re.findall(r"'([A-Z]{5})'", data)
 
-words = {w for w in words}  # removing duplicates by converting to a set
-words = sorted(list(words))
-print(f"words after removing duplicates: {len(words)}")
+    with open('sources/wordle-de.txt') as file:
+        data = file.read().replace('\n', '')
+        words += re.findall(r'"([A-Z]{5})"', data)
 
-with open('all_words.txt', 'w') as file:
-    file.write('\n'.join(words))
+    words = {w for w in words}  # removing duplicates by converting to a set
+    words = sorted(list(words))
+    print(f"words: {len(words)}")
+
+    with open('wordlist.ts', 'w') as file:
+        content = "export const WORDS = [\n" \
+                  + '\n'.join([f"'{str(s).lower()}'," for s in words]) \
+                  + "\n]"
+        file.write(content)
+
+
+def generate_solutions():
+    solutions = []
+
+    line = linecache.getline('sources/wordle-de.txt', 19399)
+    solutions += re.findall(r'"([A-Z]{5})"', line)
+    line = linecache.getline('sources/wordle-at.txt', 29)
+    solutions += re.findall(r'"([A-Z]{5})"', line)
+
+    solutions = sorted(list({s for s in solutions}))
+    print(f"solutions: {len(solutions)}")
+
+    with open('validGuesses.ts', 'w') as file:
+        content = "export const VALID_GUESSES = [\n" \
+                  + '\n'.join([f"'{str(s).lower()}'," for s in solutions]) \
+                  + "\n]"
+        file.write(content)
+
+
+if __name__ == '__main__':
+    generate_words()
+    generate_solutions()
